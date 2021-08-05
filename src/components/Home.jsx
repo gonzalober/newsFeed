7ca5e8df3ab47c1ba445669d32a7626e4732b028
gonzalober <gonzaloberhouet@gmail.com>
2021-08-05 09:42:06 +0100

    Hiperlinks

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,21 +3,37 @@ import { useState, useEffect } from "react";
 export const Home = () => {
   const [newsFeed, setNewsFeed] = useState([]);
   const [isLoading, setLoading] = useState(true);
+  const [error, setError] = useState();
 
-  const loadingFeed = async () => {
-    await fetch(
+  const loadingFeed = () => {
+    fetch(
       "https://content.guardianapis.com/search?api-key=9215094c-0a04-4ea0-ac62-b8b0bc96ca02"
     )
       .then((response) => response.json())
       .then((data) => {
         setNewsFeed(data);
         setLoading(false);
+        setError(undefined);
       })
-      .catch((error) => {
-        console.error("Error:", error);
+      .catch((er) => {
+        console.error("Error:", er);
+        setError(er.message);
+        setLoading(false);
+        setNewsFeed([]);
       });
   };
-
+  // const loadingFeed = async () => {
+  //   try {
+  //     let response = await fetch(
+  //       "https://content.guardianapis.com/search?api-key=9215094c-0a04-4ea0-ac62-b8b0bc96ca02"
+  //     );
+  //     const data = await response.json();
+  //     setNewsFeed(data);
+  //     setLoading(false);
+  //   } catch (error) {
+  //     console.error("Error:", error);
+  //   }
+  // };
   useEffect(() => {
     loadingFeed();
   }, []);
@@ -26,15 +42,17 @@ export const Home = () => {
 
   return (
     <div className="App">
+      {error && <div>{error}</div>}
       {isLoading ? (
-        <div>Loading</div>
+        <div>Loading...</div>
       ) : (
         <ul>
-          {newsFeed.response.results.map((news) => {
+          {newsFeed?.response?.results.map((news) => {
             return (
               <li key={news.id}>
-                {news.webTitle}
-                <a href={news.apiUrl}></a>
+                <a href={news.webUrl} target="_blank" rel="noopener noreferrer">
+                  {news.webTitle}
+                </a>
               </li>
             );
           })}
