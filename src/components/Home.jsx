9cb6fd4ae0745edd4e60d02fa83f4eb13f358dbe
gonzalober <gonzaloberhouet@gmail.com>
2021-08-10 22:19:51 +0100

    delete comments

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -129,37 +129,3 @@ export const Home = ({ handleNews }) => {
     </div>
   );
 };
-
-// return (
-//   <div className="App">
-//     {error && <div>{error}</div>}
-//     {isLoading ? (
-//       <div>Loading...</div>
-//     ) : (
-//       <ul>
-//         {newsFeed?.response?.results.map((news) => {
-//           return (
-//             <li key={news.id}>
-//               <a href={news.webUrl} target="_blank" rel="noopener noreferrer">
-//                 {news.webTitle}
-//               </a>
-//             </li>
-//           );
-//         })}
-//       </ul>
-//     )}
-//   </div>
-// );
-
-// const loadingFeed = async () => {
-//   try {
-//     let response = await fetch(
-//       "https://content.guardianapis.com/search?api-key=9215094c-0a04-4ea0-ac62-b8b0bc96ca02"
-//     );
-//     const data = await response.json();
-//     setNewsFeed(data);
-//     setLoading(false);
-//   } catch (error) {
-//     console.error("Error:", error);
-//   }
-// };
